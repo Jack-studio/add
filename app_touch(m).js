03f27787ebb6c3a5018d5ec2450b0840806e26e1
Jack-studio <71664690+Jack-studio@users.noreply.github.com>
2021-02-03 20:44:36 +0200

    Add files via upload

--- app_touch(m).js
+++ app_touch(m).js
@@ -1,16 +1,24 @@
-/*document.querySelector('.block one').addEventListener("touchstart", myTouch);
+let a = document.querySelector('.block_one').addEventListener("touchstart", myTouch);
+let b = document.querySelector('.block_two').addEventListener("touchstart", myTouch1);
 
 function myTouch() {
-    
-}*/
+    if (a = true) {
+    document.querySelector('.out_one').innerHTML += 'one';
+} 
+}
+function myTouch1() {
+if (b = true) {
+    document.querySelector('.out_one').innerHTML += 'two';
+}
+    }
 
 
 
-function startup() {
+/*function startup() {
     var el = document.querySelector(".block one");
     el.addEventListener("touchstart", handleStart, true);
     el.addEventListener("touchend", handleEnd, false);
     el.addEventListener("touchcancel", handleCancel, true);
     el.addEventListener("touchmove", handleMove, true);
-    document.querySelector('.out one').innerHTML += 'one';
-  }
+    
+  }*/
